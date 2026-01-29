2529c72b9cba30a9592714363c08154642e56fd7
upatthestars0 <lina.tlemcani03@gmail.com>
2026-01-29 17:46:47 +0000

    hero setup

--- lina-portfolio/app/page.tsx
+++ lina-portfolio/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/sections/Hero";
+
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1>Hellooooo welcome to my portfolioooo!</h1>
+    <main className="space-y-24">
+      <Hero />
     </main>
   );
 }
